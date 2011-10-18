c790602ef0ecd25b55dd9f599ff6c8da97d4d055
Sapan Diwakar <diwakar.sapan@gmail.com>
2011-10-19 00:48:24 +0200

    Build adjacency matrix

---
+++ src/main/java/twitter/dataanalyzer/AdjacencyMatrixBuilder.java
@@ -0,0 +1,107 @@
+package twitter.dataanalyzer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Projections;
+
+import twitter.dto.FollowerIdDto;
+import twitter.dto.FriendIdDto;
+import twitter.dto.UserDto;
+import twitter4j.conf.ConfigurationBuilder;
+import dbutils.HibernateUtil;
+
+public class AdjacencyMatrixBuilder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setOAuthAccessToken(
+				"393631018-cgeZ7fHU3EThy5ECq8MiGvNiP80BzLf6PMJk5DRT")
+				.setOAuthAccessTokenSecret(
+						"w5ByrXSNm0QGTV9UHw7gZuaJU3ZWMrHhgwswxssiA")
+				.setOAuthConsumerKey("YZLTCc6TbAEM9g63dr56Nw")
+				.setOAuthConsumerSecret(
+						"BxzTFYqF3yOg10V5nY7pBo8SdFor3YUevocJnGcrkE")
+				.setUseSSL(true).setDebugEnabled(true);
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		constructMatrix(session, transaction, UserDto.class);
+		// @SuppressWarnings("unused")
+		//
+		//
+		// Query q = session.createQuery("from UserDto");
+		// @SuppressWarnings("unchecked")
+		// List<UserDto> users = q.list();
+		//
+		// TwitterStream twitterStream = new TwitterStreamFactory(cb.build())
+		// .getInstance();
+		//
+		// TwitterStreamUtils.follow(twitterStream, users);
+
+	}
+
+	private static void constructMatrix(Session session, Transaction transaction, Class<UserDto> tableClass) {
+		int rowsCount = (int) AdjacencyMatrixBuilder.countRows(session,
+				UserDto.class);
+		boolean[][] userRelations = new boolean[rowsCount][rowsCount];
+		Map<Long, Integer> m = new HashMap<Long, Integer>();
+
+		Integer i = 0;
+		List<UserDto> users = session.createQuery("from UserDto").list();
+
+		for (UserDto u: users) {			
+			m.put(u.getId(), i);
+			i++;			
+		}
+	
+		for (UserDto u:users) {
+			Set<FollowerIdDto> followersIds = u.getFollowersIds();
+			Set<FriendIdDto> friendsIds = u.getFriendsIds();
+			
+			for (FollowerIdDto followerId : followersIds) {
+				if (m.containsKey(followerId.getId())) {
+					userRelations[m.get(followerId.getId())][m.get(u.getId())] = true;
+				}
+			}
+			
+			for (FriendIdDto friendId : friendsIds) {
+				if (m.containsKey(friendId.getId())) {
+					userRelations[m.get(u.getId())][m.get(friendId.getId())] = true;
+				}
+			}
+		}
+		try {
+			FileWriter fw = new FileWriter("userRelations.txt");
+			BufferedWriter out = new BufferedWriter(fw);
+			for (int k = 0; k < userRelations.length; ++k) {
+				for (int j = 0; j < userRelations[k].length; ++j) {
+					out.write(userRelations[k][j] + " ");
+				}
+				out.write("\n");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	public static long countRows(Session session, Class<UserDto> tableClass) {
+		return ((Number) session.createCriteria(tableClass)
+				.setProjection(Projections.rowCount()).uniqueResult())
+				.longValue();
+	}
+}
--- src/main/java/twitter/dto/MentionEntityDto.java
+++ src/main/java/twitter/dto/MentionEntityDto.java
@@ -9,9 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.GenerationTime;
-
 /**
  * @author sapan and pulkit
  * 
